8219b785880e6ec5ea5a6cf82bbb1ef4da522b89
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-10-07 12:34:42 +0545

    firebase auth insitilized

---
+++ lib/screens/forget_password.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ForegtPassword extends StatefulWidget {
+  const ForegtPassword({super.key});
+
+  @override
+  State<ForegtPassword> createState() => _ForegtPasswordState();
+}
+
+class _ForegtPasswordState extends State<ForegtPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
