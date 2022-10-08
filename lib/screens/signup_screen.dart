4865e539df7799462b2c299fb037a1f690f9d683
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-10-08 13:18:44 +0545

    cleaned

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,7 +3,6 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fiverr/helpers/global_method.dart';
 import 'package:fiverr/screens/login_screen.dart';
