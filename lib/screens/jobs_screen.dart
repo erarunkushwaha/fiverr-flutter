188263eba68b224040e85ac1783e635006d0e14a
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-10-11 08:38:02 +0545

    filter added

--- lib/screens/jobs_screen.dart
+++ lib/screens/jobs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fiverr/helpers/persistent.dart';
 import 'package:fiverr/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,94 @@ class JobsScreen extends StatefulWidget {
 }
 
 class _JobsScreenState extends State<JobsScreen> {
+  String? jobCategoryFilter;
+
+  void _selectCategory(index) {
+    setState(() {
+      jobCategoryFilter = Persistent.jobCategoryList[index];
+    });
+    Navigator.canPop(context) ? Navigator.pop(context) : null;
+  }
+
+  void showTaskCategoryDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.black54,
+            title: const Text(
+              "Job Category",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+            content: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: Persistent.jobCategoryList.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () => _selectCategory(index),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.arrow_right_alt_outlined,
+                          color: Colors.green,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            Persistent.jobCategoryList[index],
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                overflow: TextOverflow.fade),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Close",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 19,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    jobCategoryFilter = null;
+                  });
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Cancel Filter",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 19,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,6 +131,14 @@ class _JobsScreenState extends State<JobsScreen> {
           ),
           title: const Text("Jobs Screen"),
           centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.filter_list_outlined,
+              color: Colors.white,
+              size: 30,
+            ),
+            onPressed: showTaskCategoryDialog,
+          ),
         ),
       ),
     );
